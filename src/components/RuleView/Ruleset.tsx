3d03816c8c543ff521989261b8bd39ceacf680b0
KV Le <32186663+kvietcong@users.noreply.github.com>
2024-01-27 03:46:37 -0600

    🔍 Improve ruleset searching

--- src/components/RuleView/Ruleset.tsx
+++ src/components/RuleView/Ruleset.tsx
@@ -1,8 +1,15 @@
 import supabase from "../../supabase";
 import classes from "./Rule.module.css";
-import { useState, useEffect } from "react";
+import {
+    useState,
+    useEffect,
+    useMemo,
+    memo,
+    Dispatch,
+    SetStateAction,
+} from "react";
 import { useParams } from "react-router-dom";
-import { getTypeColor } from "../../util/PokemonColors";
+import { colorByType, getTypeColor } from "../../util/PokemonColors";
 
 import { Sprites, PokemonSprite } from "@pkmn/img";
 import { Dex, Species, toID } from "@pkmn/dex";
@@ -20,9 +27,12 @@ import {
     TooltipFloating,
     Input,
     Button,
+    Slider,
+    Chip,
+    Checkbox,
 } from "@mantine/core";
 import Fuse from "fuse.js";
-import { useDebouncedValue, useWindowScroll } from "@mantine/hooks";
+import { useDebouncedState, useWindowScroll } from "@mantine/hooks";
 import { Loading } from "../Loading/Loading";
 
 type Pokemon = {
@@ -75,131 +85,117 @@ const PokemonCard = ({
     );
 };
 
-export const RulesetAccordion = ({
-    rules,
-    onClick,
-}: {
-    rules: PointRule[];
-    onClick?: CardOnClick;
-}) => {
-    const [fuses, setFuses] = useState<{ [id: string]: Fuse<Pokemon> }>({});
-
-    const [search, setSearch] = useState("");
-    const [debouncedSearch] = useDebouncedValue(search, 500);
-    const [scroll, scrollTo] = useWindowScroll();
-
-    const [open, setOpen] = useState<string[]>([]);
-
-    const getTypesFrontAndBack = (pokemon: Pokemon) =>
-        pokemon.data.types.join(" ") +
-        " " +
-        pokemon.data.types.toReversed().join(" ");
-
-    useEffect(() => {
-        const newFuses = rules.reduce<{ [id: string]: Fuse<Pokemon> }>(
-            (acc, next) => {
-                const [value, pokemonData] = next;
-                acc[value] = new Fuse(pokemonData, {
-                    keys: [
-                        "data.id",
-                        "data.name",
-                        {
-                            name: "types",
-                            getFn: getTypesFrontAndBack,
-                        },
-                    ],
-                    useExtendedSearch: true,
-                    threshold: 0.4,
-                });
-                return acc;
-            },
-            {}
-        );
-        setFuses(newFuses);
-    }, [rules]);
-
-    return (
-        <>
-            <Group pos="fixed" left={25} bottom={20} style={{ zIndex: 500 }}>
-                <Button
-                    onClick={() =>
-                        setOpen(
-                            open.length
-                                ? []
-                                : Object.values(rules)
-                                      .map((x) => x[0])
-                                      .filter((x) => x != "0")
-                        )
-                    }
+export const RulesetAccordion = memo(
+    ({
+        rules,
+        open,
+        setOpen,
+    }: {
+        rules: PointRule[];
+        open?: string[];
+        setOpen?: Dispatch<SetStateAction<string[]>>;
+    }) => {
+        return (
+            <>
+                <Accordion
+                    multiple={true}
+                    variant="separated"
+                    transitionDuration={500}
+                    value={open}
+                    onChange={setOpen}
                 >
-                    {open.length ? "Close All" : "Open All"}
-                </Button>
-                <Button
-                    onClick={() => {
-                        if (scroll.y > 100) scrollTo({ y: 0 });
-                        else
-                            scrollTo({
-                                y: window.document.body.scrollHeight,
-                            });
-                    }}
-                >
-                    Go {scroll.y > 100 ? "Up" : "Down"}
-                </Button>
-            </Group>
-            <Input
-                placeholder="Search for Pokemon"
-                value={search}
-                onChange={(e) => setSearch(e.target.value)}
-            />
-            <Accordion
-                multiple={true}
-                variant="separated"
-                transitionDuration={500}
-                value={open}
-                onChange={setOpen}
-            >
-                {rules.map(([value, pokemonData]) => {
-                    const fuse = fuses[value];
-                    const results = debouncedSearch
-                        ? fuse
-                              .search(debouncedSearch.trim())
-                              .map((match) => match.item)
-                        : pokemonData;
-                    if (!results.length) return <></>;
-
-                    return (
+                    {rules.map(([value, pokemonData]) => (
                         <Accordion.Item key={value} value={value}>
                             <Accordion.Control>
                                 {value === "0" ? "Banned" : `${value} Points`}
                             </Accordion.Control>
                             <Accordion.Panel>
                                 <Group justify="center">
-                                    {results.map((pokemon) => (
+                                    {pokemonData.map((pokemon) => (
                                         <PokemonCard
                                             key={pokemon.data.id}
                                             pokemon={pokemon}
-                                            onClick={onClick}
                                         />
                                     ))}
                                 </Group>
                             </Accordion.Panel>
                         </Accordion.Item>
-                    );
-                })}
-            </Accordion>
-        </>
-    );
-};
+                    ))}
+                </Accordion>
+            </>
+        );
+    }
+);
 
 export const RulesetView = ({ ruleset }: { ruleset: number | string }) => {
     const [rules, setRules] = useState<PointRule[]>([]);
     const [rulesetName, setRulesetName] = useState<string>("");
 
+    const [name, setName] = useDebouncedState("", 300);
+    const [fuzzyLevel, setFuzzyLevel] = useDebouncedState(0.3, 300);
+
+    const [types, setTypes] = useState<string[]>([]);
+    const [willAndTypes, setWillAndTypes] = useState(true);
+
+    const [scroll, scrollTo] = useWindowScroll();
+    const [open, setOpen] = useState<string[]>([]);
+
+    const nameFuzzySearcher = useMemo(() => {
+        const names = rules.reduce<{ name: string; id: string }[]>(
+            (acc, next) => {
+                const namesAndIDs = next[1].map((pokemon) => ({
+                    name: pokemon.data.name as string,
+                    id: pokemon.data.id as string,
+                }));
+                acc.push(...namesAndIDs);
+                return acc;
+            },
+            []
+        );
+        const result = new Fuse(names, {
+            keys: ["name"],
+            threshold: fuzzyLevel,
+        });
+        return result;
+    }, [rules, fuzzyLevel]);
+
+    const filteredRules = useMemo(() => {
+        const predicates = [(_: Pokemon) => true];
+        if (name) {
+            const matchedIDs = nameFuzzySearcher
+                .search(name)
+                .map((result) => result.item.id);
+            const namePredicate = (pokemon: Pokemon) =>
+                matchedIDs.includes(pokemon.data.id);
+            predicates.push(namePredicate);
+        }
+        if (types.length) {
+            const typePredicate = (pokemon: Pokemon) => {
+                const fn = willAndTypes ? types.every : types.some;
+                return fn.bind(types)((type: string) =>
+                    pokemon.data.types
+                        .map((type) => type.toLowerCase())
+                        .includes(type)
+                );
+            };
+            predicates.push(typePredicate);
+        }
+        const doesPokemonMatch = (pokemon: Pokemon) =>
+            predicates.every((predicate) => predicate(pokemon));
+        const result = rules.reduce<PointRule[]>((acc, next) => {
+            const filteredPokemon = next[1].filter(doesPokemonMatch);
+            if (filteredPokemon.length) acc.push([next[0], filteredPokemon]);
+            return acc;
+        }, []);
+        return result;
+    }, [name, rules, types, willAndTypes, nameFuzzySearcher]);
+
     const fetchName = async (ruleset: number | string) => {
         let { data, error } = await supabase
             .from("point_rule_set")
-            .select("id, name")
-            .eq("id", ruleset);
+            .select("name")
+            .eq("id", ruleset)
+            .limit(1);
         if (error) return console.error(error);
         if (!data) return console.log("No data received!");
         setRulesetName(data[0].name);
@@ -208,36 +204,43 @@ export const RulesetView = ({ ruleset }: { ruleset: number | string }) => {
     const fetchRules = async (ruleset: number | string) => {
         let { data, error } = await supabase
             .from("point_rule")
-            .select("value, pokemon_id, point_rule_set")
+            .select("value, pokemon_id")
             .eq("point_rule_set", ruleset);
         if (error) return console.error(error);
         if (!data) return console.log("No data received!");
-        const pointRules: PointRule[] = Object.entries(
-            data.reduce<{
-                [id: string]: Pokemon[];
-            }>((acc, next: { value: number; pokemon_id: string }) => {
-                const {
-                    value,
-                    pokemon_id: rawPokemonID,
-                }: { value: number; pokemon_id: string } = next;
-                const key = value.toString();
-                const pokemonID = toID(rawPokemonID);
-                if (!acc[key]) acc[value] = [];
-                acc[key].push({
-                    data: Dex.species.getByID(pokemonID),
-                    sprite: Sprites.getDexPokemon(pokemonID, {
-                        gen: "gen5ani",
-                    }) as PokemonSprite,
-                });
-                return acc;
-            }, {})
-        ).sort((a, b) => {
+
+        const zeroThenHiToLo = (
+            a: [string, Pokemon[]],
+            b: [string, Pokemon[]]
+        ) => {
             const valA = parseInt(a[0]);
             const valB = parseInt(b[0]);
             if (valA === 0) return -1;
             else if (valB === 0) return 1;
             else return valB - valA;
-        });
+        };
+        const getPokemonFromData = (
+            accumulated: { [value: string]: Pokemon[] },
+            next: { value: number; pokemon_id: string }
+        ) => {
+            const {
+                value,
+                pokemon_id: rawPokemonID,
+            }: { value: number; pokemon_id: string } = next;
+            const key = value.toString();
+            const pokemonID = toID(rawPokemonID);
+            if (!accumulated[key]) accumulated[value] = [];
+            accumulated[key].push({
+                data: Dex.species.getByID(pokemonID),
+                sprite: Sprites.getDexPokemon(pokemonID, {
+                    gen: "gen5ani",
+                }) as PokemonSprite,
+            });
+            return accumulated;
+        };
+        const pointRules: PointRule[] = Object.entries(
+            data.reduce<{ [id: string]: Pokemon[] }>(getPokemonFromData, {})
+        ).sort(zeroThenHiToLo);
         setRules(pointRules);
     };
 
@@ -262,7 +265,74 @@ export const RulesetView = ({ ruleset }: { ruleset: number | string }) => {
                         {rulesetName}
                     </Text>
                 </Title>
-                <RulesetAccordion rules={rules} />
+                <Input
+                    defaultValue={name}
+                    placeholder="Search for Pokemon"
+                    onChange={(e) => setName(e.target.value)}
+                />
+                <Group>
+                    Name Fuzzing (0 perfect match, 1 super fuzz):{" "}
+                    <Slider
+                        min={0}
+                        max={1}
+                        step={0.05}
+                        w="100%"
+                        defaultValue={fuzzyLevel}
+                        onChange={setFuzzyLevel}
+                    />
+                </Group>
+                <Group justify="center">
+                    <Chip.Group multiple value={types} onChange={setTypes}>
+                        {Object.entries(colorByType).map(([type, color]) => (
+                            <Chip color={color} key={type} value={type}>
+                                {type}
+                            </Chip>
+                        ))}
+                        <Checkbox
+                            checked={willAndTypes}
+                            onChange={(e) =>
+                                setWillAndTypes(e.currentTarget.checked)
+                            }
+                            label="Match All Types"
+                        />
+                    </Chip.Group>
+                </Group>
+                <RulesetAccordion
+                    open={open}
+                    setOpen={setOpen}
+                    rules={filteredRules}
+                />
+                <Group
+                    pos="fixed"
+                    left={25}
+                    bottom={20}
+                    style={{ zIndex: 500 }}
+                >
+                    <Button
+                        onClick={() =>
+                            setOpen(
+                                open.length
+                                    ? []
+                                    : Object.values(rules)
+                                        .map((x) => x[0])
+                                        .filter((x) => x != "0")
+                            )
+                        }
+                    >
+                        {open.length ? "Close All" : "Open All"}
+                    </Button>
+                    <Button
+                        onClick={() => {
+                            if (scroll.y > 100) scrollTo({ y: 0 });
+                            else
+                                scrollTo({
+                                    y: window.document.body.scrollHeight,
+                                });
+                        }}
+                    >
+                        Go {scroll.y > 100 ? "Up" : "Down"}
+                    </Button>
+                </Group>
             </Stack>
         </Center>
     );
