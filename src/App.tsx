6d109800dedde7575320542679d422194777fd08
KV Le <32186663+kvietcong@users.noreply.github.com>
2024-05-22 20:17:27 -0700

    🔍 Move Pokemon move retrieval to TanStack Query

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ const queryClient = new QueryClient({
     defaultOptions: {
         queries: {
             staleTime: 5 * 60 * 1000,
+            gcTime: 24 * 60 * 60 * 1000,
         },
     },
 });
